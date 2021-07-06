7882a506e2221c6a3158e2c559c08becbd397545
Ben <bpk@koinegreek.com>
2021-07-06 17:48:35 +0100

    capitalizationmg

--- modern_greek_phonemic_phonetic.py
+++ modern_greek_phonemic_phonetic.py
@@ -85,6 +85,8 @@ greek_patterns_case_and_stress = [
 
 ##making_everything_lowercase_START###
 
+    [re.compile("Α"), "α"],
+
     [re.compile("Β"), "β"],
     [re.compile("Γ"), "γ"],
     [re.compile("Δ"), "δ"],
