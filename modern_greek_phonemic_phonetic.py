038f5aae5cc505e4764d3472eb38b90fa6de8c0f
Ben <bpk@koinegreek.com>
2021-07-06 22:21:15 +0100

    test

--- modern_greek_phonemic_phonetic.py
+++ modern_greek_phonemic_phonetic.py
@@ -776,7 +776,7 @@ def remove_acute(word):
 
 def main(word):
 
-    phonemic1 = print(word.split(" "))
+    phonemic1 = print(word.split())
 
     phonemic = transcribed_phonemic(transcribed_case_and_stress(phonemic1))
     phonetic = remove_acute(convert_stress(transcribed_phonetic(transcribed_case_and_stress(word))))
