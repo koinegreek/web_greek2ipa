771b6cf6077887085087f35b26c646b64236dfff
Ben <bpk@koinegreek.com>
2021-07-06 23:06:09 +0100

    test

--- modern_greek_phonemic_phonetic.py
+++ modern_greek_phonemic_phonetic.py
@@ -776,7 +776,7 @@ def split_time(word):
 
 def main(words):
 
-    phonemic = split_time(word)
+    phonemic = split_time(words)
     phonetic = remove_acute(convert_stress(transcribed_phonetic(transcribed_case_and_stress(words))))
 
     return phonemic, phonetic
