18029e50a78647780fdb62b38e2b24052e510939
Ben <bpk@koinegreek.com>
2021-07-06 21:39:54 +0100

    split test

--- modern_greek_phonemic_phonetic.py
+++ modern_greek_phonemic_phonetic.py
@@ -773,7 +773,9 @@ def remove_acute(word):
 
 def main(words):
 
-    phonemic = "/"+transcribed_phonemic(transcribed_case_and_stress(words)).split()+"/"
+    phonemic = txt.split()
+
+    txt = transcribed_phonemic(transcribed_case_and_stress(words))
     phonetic = "["+remove_acute(convert_stress(transcribed_phonetic(transcribed_case_and_stress(words))))+"]"
 
 
