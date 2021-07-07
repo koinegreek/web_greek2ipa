3f9d600add92a18c4b71b5e09d00dc65378abd5d
Ben <bpk@koinegreek.com>
2021-07-07 09:10:14 +0100

    τεστ

--- modern_greek_phonemic_phonetic.py
+++ modern_greek_phonemic_phonetic.py
@@ -778,11 +778,6 @@ def main(word):
 
     phonemic = transcribed_phonemic(transcribed_case_and_stress(word))
     phonetic1 = greek_split(word)
-
-    for x in phonetic1:
-        return remove_acute(convert_stress(transcribed_phonetic(transcribed_case_and_stress(x))))
     ##remove_acute(convert_stress(transcribed_phonetic(transcribed_case_and_stress(word))))
 
-    phonetic = x
-    
     return phonemic, phonetic
