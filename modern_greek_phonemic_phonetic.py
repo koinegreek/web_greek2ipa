83424cf67a9ac532029604f45ae09b5b6528e16a
Ben <bpk@koinegreek.com>
2021-07-07 09:31:13 +0100

    τεστ

--- modern_greek_phonemic_phonetic.py
+++ modern_greek_phonemic_phonetic.py
@@ -777,13 +777,12 @@ def greek_split(word):
 def greek_split2(word):
     xyz = greek_split(word)
     for word in xyz:
-        remove_acute(convert_stress(transcribed_phonetic(transcribed_case_and_stress(word))))
         return word
 
 def main(word):
 
     phonemic = transcribed_phonemic(transcribed_case_and_stress(word))
-    phonetic = greek_split2(word)
+    phonetic = greek_split(word)
     ##remove_acute(convert_stress(transcribed_phonetic(transcribed_case_and_stress(word))))
 
     return phonemic, phonetic
