28dce4b02bc635269456154070d566a2b41a6a7d
Ben <bpk@koinegreek.com>
2021-07-08 09:16:50 +0100

    sandhi3

--- modern_greek_phonemic_phonetic.py
+++ modern_greek_phonemic_phonetic.py
@@ -497,7 +497,7 @@ def iterate_phonemic(word):
 def iterate_phonetic(word):
 
     a_list = greek_split(word)
-    new_list = [sandhi(remove_acute(convert_stress(transcribed_phonetic(transcribed_phonemic(pp_phonetic(transcribed_case_and_stress(element))))))) for element in a_list]
+    new_list = [remove_acute(convert_stress(transcribed_phonetic(transcribed_phonemic(pp_phonetic(transcribed_case_and_stress(element)))))) for element in a_list]
     back_to_string = (" ".join(new_list))
 
     return(back_to_string)
@@ -505,7 +505,7 @@ def iterate_phonetic(word):
 def main(word):
 
     phonemic = "/"+iterate_phonemic(word)+"/"
-    phonetic = "["+iterate_phonetic(word)+"]"
+    phonetic = "["+sandhi(iterate_phonetic(word))+"]"
     ##remove_acute(convert_stress(transcribed_phonetic(transcribed_case_and_stress(word))))
 
     return phonemic, phonetic
